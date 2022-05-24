4c5cca815a1c7ae8b673c706d2f1d595f3d500dd
dariia89 <alefed89@mail.ru>
2022-05-24 11:55:19 +0300

    ввыполнила задачи из третьего домашнего задания

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const arr = path.split('.');
+  return function getter(obj) {
+    if (obj.hasOwnProperty(arr[0])) {
+      if (obj[arr[0]] !== Object(obj[arr[0]])) {
+        return obj[arr[0]];
+      } else {
+        const prop = arr.splice(0, 1);
+        return getter(obj[prop]);
+      }
+    } else {
+      return;
+    }
+  };
 }
